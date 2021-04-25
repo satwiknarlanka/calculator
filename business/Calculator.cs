3e6dbff9a27d998359f9a837c1cb23ae6803f804
Satwik Narlanka <9220329+satwiknarlanka@users.noreply.github.com>
2021-04-25 14:41:02 +0530

    Logic for simple expression
    Write logic for expression with only numbers and + - operators

--- business/Calculator.cs
+++ business/Calculator.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace business
 {
     public class Calculator
     {
+        /// <summary>
+        /// Evaluates the mathematical expression.
+        /// Assumes the expression will not evalute to a decimal.
+        /// No rounding of expression has been taken care of.
+        /// </summary>
+        /// <param name="expression">string expression to evaluate</param>
+        /// <returns>evaulated result</returns>
         public int Calculate(string expression)
         {
-            throw new NotImplementedException();
+            var result = EvaluatePlusMinus(expression.ToCharArray());
+            return result;
+        }
+
+        private int EvaluatePlusMinus(char[] subExpression)
+        {
+            var computedValue = 0;
+            var number = new List<char>();
+            var operation = 1;
+            foreach(var element in subExpression)
+            {
+                if(element == '+')
+                {
+                    computedValue += int.Parse(number.ToArray()) * operation;;
+                    operation = 1;
+                    number = new List<char>();
+                }
+                else if(element == '-')
+                {
+                    computedValue += int.Parse(number.ToArray()) * operation;
+                    operation = -1;
+                    number = new List<char>();
+                }
+                else
+                {
+                    number.Add(element);
+                }
+            }
+            
+            computedValue += int.Parse(number.ToArray()) * operation;
+            return computedValue;
         }
     }
 }
--- tests/CalculatorTests.cs
+++ tests/CalculatorTests.cs
@@ -11,14 +11,14 @@ namespace tests
         public void Calculator_GivenOnlyOperatorsPlusMinus_ReturnsEvaluatedExpression_Success()
         {
             // Arrange
-            var expression = "2+3-1";
+            var expression = "2+3-1+4";
 
             // Act
             var sut = new Calculator();
             var result = sut.Calculate(expression);
 
             // Assert
-            result.Should().Be(4);
+            result.Should().Be(8);
         }
     }
 }
